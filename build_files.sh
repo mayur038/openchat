59eccb31238cc59791ee25a8215723e72791ad62
mayur038 <uniquecoder010@gmail.com>
2024-07-20 03:02:05 +0530

    ni

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,3 @@
-python3 -m pip install -r requirements.txt && python3 manage.py collectstatic --noinput && python3 manage.py migrate
+python3 -m pip install -r requirements.txt;
+python3 manage.py makemigrations;
+python3 manage.py migrate;
