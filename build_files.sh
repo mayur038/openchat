39e66c9631c2c550c821759e010e727e0a1b48a4
Mayur Gohil <84913465+mayur038@users.noreply.github.com>
2024-07-20 02:08:30 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,1 @@
-python3 -m pip install -r requirements.txt && python3 manage.py collectstatic --noinput && pip3 install pysqlite3 && python3 manage.py migrate
+python3 -m pip install -r requirements.txt && python3 -m pip install pysqlite3 && python3 manage.py collectstatic --noinput && python3 manage.py migrate
