d7ac2900c9d2299fffd73071e005aa6dfc57ce0c
Mayur Gohil <84913465+mayur038@users.noreply.github.com>
2024-07-20 01:51:39 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,1 @@
-python3 -m pip install -r requirements.txt && python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py collectstatic 
+python3 -m pip install -r requirements.txt && python3 manage.py collectstatic --noinput && python3 manage.py migrate
